f7d5a9a500262b325c6670b87ee3e7ccaf7f386e
mspegagne <mathieu.sge@gmail.com>
2014-06-03 20:32:53 +0200

    0.7.1
    bug en moins

--- classes/Ouvrage.php
+++ classes/Ouvrage.php
@@ -107,26 +107,20 @@ class Ouvrage
 
 	$champs = $this->champs;
 	$values = $this->values;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-	?>	<div class="row" style="margin-left: 15px;">
+
+	?>	
 		<div class="col-lg-10 panel panel-info">
 <?php
 	$isbn ="";
 	
-	?>	
-		<div class="row" style="margin-left: 15px;">
-			<div class="col-lg-10 panel panel-info">
-				<div class="col-lg-9 col-lg-push-3">
-	<?php
 
 	foreach($values as $cle =>$valeur)
 	{
 		if((!is_numeric($cle)) AND $cle!='id')
 		{
 			?>
-			<div class="panel-body"><?php echo "<strong>".$cle."</strong>"; ?> : <?php 
+			<?php echo "<strong>".$cle."</strong>"; ?> : <?php 
 			if(filter_var($valeur, FILTER_VALIDATE_URL))
 			{
 				echo "<a href=".$valeur." target=_blank>".$valeur."</a>";
@@ -144,62 +138,30 @@ class Ouvrage
 			}
 			elseif($cle=="Title"||$cle=="Titre"||$cle=="title"||$cle=="titre")
 			{
-				echo "<strong style='color:red;'>".$valeur."</strong>";
+				echo "<strong style='color:red;'>".$valeur."</strong><br />";
 			}
 			elseif($cle=="ISBN"||$cle=="isbn")
 			{
-				echo $valeur;
+				echo $valeur.'<br />';
 				echo '<img src="images.amazon.com/images/P/'.$valeur.'.01.SZZZZZZZ.jpg" alt="couverture"/>';
+			}
+			elseif(filter_var($valeur, FILTER_VALIDATE_URL))
+			{
+				echo "<a href=".$valeur.">".$valeur."</a>";
+			}
+			elseif($cle=="Title"||$cle=="Titre"||$cle=="title"||$cle=="titre")
+			{
+				echo "<strong style='color: rgb(180, 46 , 69);'>".$valeur."</strong><br />";
 			}
 			else
 			{
-				echo $valeur;
-			}
-			?>
-			</div>
-<?php
-				$isbn = $isbn.$expl_isbn[$i];
-				}
-				
-				}
-				elseif(filter_var($valeur, FILTER_VALIDATE_URL))
-				{
-					echo "<a href=".$valeur.">".$valeur."</a>";
-				}
-				elseif($cle=="Title"||$cle=="Titre"||$cle=="title"||$cle=="titre")
-				{
-					echo "<strong style='color: rgb(180, 46 , 69);'>".$valeur."</strong>";
-				}
-				else
-				{
-					echo $valeur;
-				}
-				?></div>
-			<?php		
+				echo $valeur.'<br />';
+			}		
 		}
 	}
 	?>
 		
-	</div>
-	</div>
-				</div>
-				<div class="col-lg-3 col-lg-pull-9" style="margin-top: 45px;">
-				<?php
-				if(filter_var("http://images.amazon.com/images/P/'.$isbn.'.01.LZZZZZZZ.jpg", FILTER_VALIDATE_URL))
-				// mauvais test : il faudrait tester si l'url est valide
-				{
-	
-					//echo '<img src="http://images.amazon.com/images/P/'.$isbn.'.01.LZZZZZZZ.jpg" alt="couverture" align="center"/>';
-					echo '<img src="http://images.amazon.com/images/P/2765414173.01.LZZZZZZZ.jpg" alt="couverture" align="center"/>';
-					// pour le moment on affiche une couverture exemple
-				}
-				else
-				{
-					echo '<img src="images\default_couv.jpg" title="couverture non-disponible" align="center"/>';
-				}
-				?>
-				</div>
-			</div>
+				
 		</div>
 	<?php
 	}
